cfa3759f9c33e60c46dc118af50e8a3ec85e4b03
kairo <kairosuki@gmail.com>
2025-08-25 10:52:04 +0200

    ok

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -61,7 +61,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to product_url(@product), notice: "Product was successfully created." }
+        format.html { redirect_to utilities_etichette_path, notice: "Aggiunta." }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -74,7 +74,7 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to utilities_etichette_path, notice: "Aggiornato." }
+        format.html { redirect_to utilities_etichette_path(:id => @product.id), notice: "Aggiornato." }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit, status: :unprocessable_entity }

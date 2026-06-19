81c037090a152838ba081fe6c9925d895ea0a911
cgv <sargassi@gmail.com>
2026-06-19 13:32:40 +0200

    feat: add real gencode columns to warehouses/locations with backfill

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -4,16 +4,14 @@ class Warehouse < ApplicationRecord
   after_create_commit :generate_qr_code
   before_update :regenerate_qr, if: :code_changed?
 
-  def gencode
-    "#{id}_#{code.gsub(/\s+/, '')}"
-  end
-
   def generate_qr_code
     require 'rqrcode'
-    update_columns(qrcode_svg: RQRCode::QRCode.new(gencode).as_svg(module_size: 6, use_path: true, viewbox: true).sub(/^<\?xml[^>]*>/, ""))
+    self.gencode = "#{id}_#{code}"
+    update_columns(gencode: self.gencode, qrcode_svg: RQRCode::QRCode.new(gencode).as_svg(module_size: 6, use_path: true, viewbox: true).sub(/^<\?xml[^>]*>/, ""))
   end
 
   def regenerate_qr
+    self.gencode = "#{id}_#{code}"
     self.qrcode_svg = RQRCode::QRCode.new(gencode).as_svg(module_size: 6, use_path: true, viewbox: true).sub(/^<\?xml[^>]*>/, "")
   end
 end

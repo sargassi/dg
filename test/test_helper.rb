86a7fb838fab83713e23a3d6d4750058a3f6f6c2
cgv <sargassi@gmail.com>
2026-07-14 16:59:18 +0200

    fix: update test fixtures and test_helper for archive import tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,8 +6,7 @@ class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  fixtures :users, :collections, :items, :warehouses, :locations, :operationtypes, :inventories, :itemins, :itemouts
 
   # Add more helper methods to be used by all tests here...
 end
